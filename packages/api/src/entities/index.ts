f36c1f14e29be3e8a8868d99db09df50e56c2a73
Ghislain Damou <ghislain.damou@epsi.fr>
2023-02-07 18:08:23 +0100

    refacto: index for importing all entities to mikro orm options

---
+++ packages/api/src/entities/index.ts
@@ -0,0 +1,23 @@
+import { Category } from "./Category";
+import { CategoryExercise } from "./CategoryExercise";
+import { Exercise } from "./Exercise";
+import { ExerciseSuperset } from "./ExerciseSuperset";
+import { Result } from "./Result";
+import { Superset } from "./Superset";
+import { User } from "./User";
+import { Workout } from "./Workout";
+import { WorkoutExercise } from "./WorkoutExercise";
+import { WorkoutSuperset } from "./WorkoutSuperset";
+
+export const entities = [
+    Category,
+    CategoryExercise,
+    Exercise,
+    ExerciseSuperset,
+    Result,
+    Superset,
+    User,
+    Workout,
+    WorkoutExercise,
+    WorkoutSuperset,
+];
