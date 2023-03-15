e855d123d4181e3ef5dbc785206e2c2cac7990cc
Ghislain Damou <ghislain.damou@epsi.fr>
2023-03-15 23:20:17 +0100

    wip: exercise router

--- packages/api/src/router/routers/exercise.ts
+++ packages/api/src/router/routers/exercise.ts
@@ -1,10 +1,40 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
 import { Exercise } from "../../entities/Exercise";
 import { publicProcedure, router } from "../trpc";
 
 export const exerciseRouter = router({
     list: publicProcedure.query(({ ctx }) => {
-        // [..]
-        const fork = ctx.orm.em.fork();
-        return fork.find(Exercise, {});
+        return ctx.orm.find(Exercise, {});
     }),
+    byId: publicProcedure
+        .input(
+            z.object({
+                id: z.string().uuid(),
+            })
+        )
+        .query(async ({ input, ctx }) => {
+            const foundExercise = await ctx.orm.find(Exercise, { id: input.id });
+
+            if (!foundExercise)
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: `exercise "${input.id}" not found`,
+                });
+
+            return foundExercise;
+        }),
+    // create: publicProcedure
+    //     .input(
+    //         z.object({
+    //             name: z.string().nonempty(),
+    //             categoriesIds: z.string().uuid().array(),
+    //         })
+    //     )
+    //     .mutation(async ({ input, ctx }) => {
+    //         const foundCategories =  ctx.orm.find(Category, input.categoriesIds);
+
+    //         ctx.orm.
+    //     }),
 });
