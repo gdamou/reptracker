94d021d5f8cbb1c86bb4cc7e002742aba55c2801
Ghislain Damou <ghislain.damou@epsi.fr>
2023-03-19 23:09:29 +0100

    feat: superset router

--- packages/api/src/router/routers/_app.ts
+++ packages/api/src/router/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { categoryRouter } from "./category";
 import { exerciseRouter } from "./exercise";
+import { supersetRouter } from "./superset";
 
 export const appRouter = router({
     category: categoryRouter,
     exercise: exerciseRouter,
+    superset: supersetRouter,
 });
 
 export type AppRouter = typeof appRouter;
